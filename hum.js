c176433a5f198bab221f6b2fdceccb9f4cf341ae
Akky830 <ryoutokurpg@gmail.com>
2021-01-29 14:58:47 +0900

    ハンバーガーメニュー追加

---
+++ hum.js
@@ -0,0 +1,8 @@
+$(function() {
+    $('#hum-menu').click(function() {
+      $('#hum-nav').fadeIn();
+    });
+    $('.close-modal').click(function(){
+        $('#hum-nav').fadeOut();
+    });
+});    
